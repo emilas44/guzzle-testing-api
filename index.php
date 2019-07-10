0a3e9b00dedd02b26532c04c5811cec2ebdaddaa
emilas <e.milas@euroconsult.de>
2019-07-10 15:17:27 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+    $apiToken = '';
+    $url = '';
+    $client = new Client();
+
+    $requestParameters = [
+        'verify' => false, // this line is only for testing server
+        'headers' => [
+            'Authorization' => 'Bearer '. $apiToken,
+            'Key' => ''
+        ],
+    ];
+
+    try {
+        $response = $client->request('GET', $url, $requestParameters);
+    } catch (GuzzleException $e) {
+        die($e->getMessage());
+    }
+
+header('Content-Type: application/json');
+echo $response->getBody()->getContents();
